145ffc90ae07c3be4f0e378079dee41a5aa08564
Casper Schippers <c.f.schippers@tue.nl>
2018-09-27 11:58:25 +0200

    added constants

--- python_tools/fnatools/llg_model_solver/llg_model.py
+++ python_tools/fnatools/llg_model_solver/llg_model.py
@@ -3,6 +3,13 @@ from .solvers_steppers import *
 
 
 class LLG_Model():
+    # physical constants
+    mu_0 = 4 * np.pi * 1e-7
+    e_charge = 1.6021766208e-19
+    h_bar = 6.626070040e-34
+    e_mass = 9.10938356e-31
+    mu_B = 0.5 * e_charge / e_mass * h_bar
+
     def __init__(self, stepper='RK4', adaptive_solver=False):
 
         if adaptive_solver:
@@ -18,6 +25,11 @@ class LLG_Model():
         self._fun = self.equations
 
     def setup(self, number_of_spins=1, exchange=-1.):
+        # define constants
+        self.g_factor = 2
+        self.gamma = self.g_factor * self.mu_B / self.h_bar
+        self.alpha = 0.02
+
         # Define spin-matrix
         m = []
         for idx in range(number_of_spins):
@@ -30,7 +42,7 @@ class LLG_Model():
         else:
             ones = np.ones((number_of_spins, number_of_spins)) - \
                 np.eye(number_of_spins)
-            self.J = ones * J
+            self.J = ones * exchange
 
     def initialize(self, ):
         pass
@@ -54,5 +66,17 @@ class LLG_Model():
         self.t_result = np.array(T)
         self.m_result = np.array(Y)
 
-    def equations(self, ):
-        pass
+    def equations(self, m, t, rand=None):
+        Heff = self.J * m
+
+        c = self.gamma / (1 + self.alpha**2) * self.mu_0
+        dm = - c * self._cross_product(m, Heff)
+
+        return dm
+
+    @staticmethod
+    def _cross_product(A, B):
+        X = A[:, 1] * B[:, 2] - A[:, 2] * B[:, 1]
+        Y = A[:, 2] * B[:, 0] - A[:, 0] * B[:, 2]
+        Z = A[:, 0] * B[:, 1] - A[:, 1] * B[:, 0]
+        return np.array([X, Y, Z]).T
