5ba145f21d2288d1d5805e0bbfada82ee33fe3b5
Casper Schippers <c.f.schippers@tue.nl>
2019-01-25 19:54:01 +0100

    progress

--- python_tools/fnatools/llg_model_solver/llg_model.py
+++ python_tools/fnatools/llg_model_solver/llg_model.py
@@ -1,14 +1,21 @@
-import numpy as np
+import numpy
+from scipy import integrate
 from .solvers_steppers import *
 
 
 class LLG_Model():
     # physical constants
-    mu_0 = 4 * np.pi * 1e-7
-    e_charge = 1.6021766208e-19
-    h_bar = 6.626070040e-34
-    e_mass = 9.10938356e-31
-    mu_B = 0.5 * e_charge / e_mass * h_bar
+    mu_0 = 4 * numpy.pi * 1e-7  # T / (A / m)
+    e_charge = 1.6021766208e-19  # C
+    h_bar = 6.626070150e-34 / (2 * numpy.pi)  # J * s
+    e_mass = 9.10938356e-31  # kg
+    mu_B = e_charge * h_bar / (2 * e_mass)  # J / T
+
+    # material constants (Cobalt as example)
+    g_factor = 2
+    gamma = g_factor * mu_B / h_bar  # Hz / T
+    alpha = 0.02
+    Ms = 1.4e6  # A / m
 
     def __init__(self, stepper='RK45', adaptive_solver=True):
         self._solver = Solver
@@ -21,29 +28,23 @@ class LLG_Model():
 
         self._fun = self.equations
 
-    def setup(self, number_of_spins=1, exchange=-0.0001):
+    def setup(self, number_of_spins=1):
         self.number_of_spins = number_of_spins
-
-        # define constants
-        self.g_factor = 2
-        self.gamma = self.g_factor * self.mu_B / self.h_bar
-        self.alpha = 0.02
-        self.Ms = 1.4e6
 
         # Define spin-matrix
         m = []
 
         for idx in range(number_of_spins):
-            m.append([np.pi * 0, np.pi / 2])
+            m.append([numpy.pi * 0, numpy.pi / 2])
 
-        self.mspheric = np.array(m).T
+        self.mspheric = numpy.array(m).T
 
         # Define exchange interaction J
-        # if type(exchange) == np.ndarray:
+        # if type(exchange) == numpy.ndarray:
         #     self.J = exchange
         # else:
-        #     ones = np.ones((number_of_spins, number_of_spins)) - \
-        #         np.eye(number_of_spins)
+        #     ones = numpy.ones((number_of_spins, number_of_spins)) - \
+        #         numpy.eye(number_of_spins)
         #     self.J = ones * exchange
 
     def initialize(self, ):
@@ -55,22 +56,28 @@ class LLG_Model():
     def execute(self, should_stop=None):
         # Prepare solving
         t = 0
-        # y = np.array([[0, 0, 1], [0, 0, 1]])
+        # y = numpy.array([[0, 0, 1], [0, 0, 1]])
         # h = 0.0001
-        t_max = 1
+        t_max = 1e-9
         h = t_max / 15000
         mspheric = self.mspheric
-        random_shape = np.shape(mspheric)
+        random_shape = numpy.shape(mspheric)
 
         # Start solver
         T, M, R = self._solver(self, t, mspheric, h, random_shape=random_shape,
                                should_stop=should_stop, t_max=t_max,
-                               step_fcn_pre=self._rotate_pre,
-                               step_fcn_post=self._rotate_post,
-                               step_fcn_requirement=self._require_rotation,
+                               # step_fcn_pre=self._rotate_pre,
+                               # step_fcn_post=self._rotate_post,
+                               # step_fcn_requirement=self._require_rotation,
                                **self._solver_kwargs)
+        # def fun(t, m):
+        #     return self.equations(t, m, )
+        # sol = integrate.solve_ivp(fun, (0, t_max), mspheric, vectorized=True)
 
-        # M = np.array(M)
+        # M = sol.y
+        # T = sol.t
+
+        # M = numpy.array(M)
 
         self.m = M[-1]
 
@@ -84,18 +91,33 @@ class LLG_Model():
         self.mcartesian_result = numpy.transpose(
             self._spherical_to_cartesian(1, theta, phi), (1, 0, 2))
 
-    def equations(self, t, mspheric, h, rand=None):
+    def equations(self, t, mspheric, h=0, rand=None):
         theta = mspheric[0, :]
         phi = mspheric[1, :]
 
-        Hfx = 1e-1
-        Hfy = 0
+        Hfx = 0
+        Hfy = 1e6
         Hfz = 0
 
         Hdx = 0
         Hdy = 0
         Hdz = 0
 
+        # Rotate axes if required
+        require_rotation = self._require_rotation(mspheric)
+        if require_rotation:
+            theta_0 = theta
+            phi_0 = phi
+
+            theta, phi = self._rotate_1qy_spherical(theta_0, phi_0,
+                                                    direction=+1)
+            Hfx, Hfy, Hfz = self._rotate_1qy_cartesian(Hfx, Hfy, Hfz,
+                                                       direction=+1)
+            Hdx, Hdy, Hdz = self._rotate_1qy_cartesian(Hdx, Hdy, Hdz,
+                                                       direction=+1)
+        
+
+        # Calculate difference in current axes
         dtheta = self.gamma * self.mu_0 * (
             - Hfx * numpy.cos(theta) +
             + Hfy * numpy.sin(theta) +
@@ -112,12 +134,16 @@ class LLG_Model():
             - self.alpha * Hdy * numpy.sin(phi)
         )
 
+        # Convert difference to original axes (if required)
+        if require_rotation:
+
+
         # print(dtheta, dphi)
 
         # dtheta = 1 + theta * 0
         # dphi = 0 + theta * 0
 
-        dmspheric = np.array([dtheta, dphi])
+        dmspheric = numpy.array([dtheta, dphi])
         return dmspheric
 
     # def equations(self, t, m, h, rand=None):
@@ -160,13 +186,13 @@ class LLG_Model():
         x, z = direction * z, - direction * x
         return x, y, z
 
-    def _rotate_pre(self, y):
-        y = np.array(self._rotate_1qy_spherical(y[0, :], y[1, :], +1))
-        return y
+    # def _rotate_pre(self, y):
+    #     y = numpy.array(self._rotate_1qy_spherical(y[0, :], y[1, :], +1))
+    #     return y
 
-    def _rotate_post(self, y):
-        y = np.array(self._rotate_1qy_spherical(y[0, :], y[1, :], -1))
-        return y
+    # def _rotate_post(self, y):
+    #     y = numpy.array(self._rotate_1qy_spherical(y[0, :], y[1, :], -1))
+    #     return y
 
     @staticmethod
     def _require_rotation(y):
@@ -180,11 +206,11 @@ class LLG_Model():
         x = r * numpy.sin(theta) * numpy.cos(phi)
         y = r * numpy.sin(theta) * numpy.sin(phi)
         z = r * numpy.cos(theta)
-        return np.array([x, y, z])
+        return numpy.array([x, y, z])
 
     @staticmethod
     def _cross_product(A, B):
         X = A[:, 1] * B[:, 2] - A[:, 2] * B[:, 1]
         Y = A[:, 2] * B[:, 0] - A[:, 0] * B[:, 2]
         Z = A[:, 0] * B[:, 1] - A[:, 1] * B[:, 0]
-        return np.array([X, Y, Z]).T
+        return numpy.array([X, Y, Z]).T
