b413acbf21bef153d9a3c2c63ebc4523a92181f2
Casper Schippers <c.f.schippers@tue.nl>
2019-04-10 15:37:06 +0200

    Added test-file

---
+++ python_tools/testfiles/pymeasure_test.py
@@ -0,0 +1,78 @@
+import logging
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+import sys
+import tempfile
+import random
+from time import sleep
+from pymeasure.log import console_log
+from pymeasure.display.Qt import QtGui, QtCore
+from pymeasure.display.windows import ManagedWindow
+from pymeasure.experiment import Procedure, Results
+from pymeasure.experiment import IntegerParameter, FloatParameter, Parameter
+
+from fnatools.pymeasure_addons import Sequencer
+
+class RandomProcedure(Procedure):
+
+    iterations = IntegerParameter('Loop Iterations')
+    delay = FloatParameter('Delay Time', units='s', default=0.2)
+    seed = Parameter('Random Seed', default='12345')
+
+    DATA_COLUMNS = ['Iteration', 'Random Number']
+
+    def startup(self):
+        log.info("Setting the seed of the random number generator")
+        random.seed(self.seed)
+
+    def execute(self):
+        log.info("Starting the loop of %d iterations" % self.iterations)
+        for i in range(self.iterations):
+            data = {
+                'Iteration': i,
+                'Random Number': random.random()
+            }
+            self.emit('results', data)
+            log.debug("Emitting results: %s" % data)
+            sleep(self.delay)
+            if self.should_stop():
+                log.warning("Caught the stop flag in the procedure")
+                break
+
+
+class MainWindow(ManagedWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__(
+            procedure_class=RandomProcedure,
+            inputs=['iterations', 'delay', 'seed'],
+            displays=['iterations', 'delay', 'seed'],
+            x_axis='Iteration',
+            y_axis='Random Number'
+        )
+        self.sequencer = Sequencer(
+            self.procedure_class,
+            self.displays,
+            parent=self
+        )
+
+        self.setWindowTitle('GUI Example')
+
+    def queue(self, *args, procedure=None):
+        filename = tempfile.mktemp()
+
+        if procedure is None:
+            procedure = self.make_procedure()
+
+        results = Results(procedure, filename)
+        experiment = self.new_experiment(results)
+
+        self.manager.queue(experiment)
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
