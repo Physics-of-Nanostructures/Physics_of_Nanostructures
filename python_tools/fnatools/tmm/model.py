0de2ca9e0a4a584875deffa9fb9010d6813a5616
Casper Schippers <c.f.schippers@tue.nl>
2019-07-17 18:43:22 +0200

    Started on selective integration

--- python_tools/fnatools/tmm/model.py
+++ python_tools/fnatools/tmm/model.py
@@ -1,8 +1,10 @@
 from tmm.tmm_core import (coh_tmm, position_resolved,
                           find_in_structure_with_inf)
 
-import numpy as np
-import matplotlib.pyplot as plt
+import numpy
+import pandas
+from scipy import integrate
+from matplotlib import pyplot
 from dataclasses import dataclass, field
 
 
@@ -57,20 +59,23 @@ class TMM_stack:
         self.layers_thickness = []
         self.layers_refractive = []
 
-        for layer in full_stack:
+        for idx, layer in enumerate(full_stack):
             material = layer[0]
             index = layer[-1]
 
             if len(layer) == 3:
                 thickness = layer[1]
             else:
-                thickness = np.inf
+                thickness = numpy.inf
+
+            label = material + f"_{idx:04d}"
 
             self.layers_materials.append(material)
             self.layers_thickness.append(thickness)
             self.layers_refractive.append(index)
+            self.layers_label.append(label)
 
-        self.total_thickness = np.sum([l[1] for l in self.stack])
+        self.total_thickness = numpy.sum([l[1] for l in self.stack])
         self.materials = list(set(self.layers_materials))
 
         self._prepared_layers = True
@@ -86,33 +91,47 @@ class TMM_stack:
             self.θ0, self.λ0
         )
 
-        self.T_step = 0.01
-        self.T = np.arange(-self.plot_margin_l,
-                           self.total_thickness + self.plot_margin_r +
-                           self.T_step,
-                           self.T_step)  # position in structure
+        self.d_step = 0.01
+        self.d_res = pandas.DataFrame()
+        self.d_res["x"] = numpy.arange(
+            -self.plot_margin_l,
+            self.total_thickness + self.plot_margin_r + self.d_step,
+            self.d_step
+        )
 
-        self.absorption = np.zeros(len(self.T))
-        for idx, t in enumerate(self.T):
-            self.absorption[idx] = (
-                position_resolved(
-                    *find_in_structure_with_inf(self.layers_thickness, t),
-                    coh_tmm_data
-                )['absor']
-            )
+        self.d_res["absorption"] = self.d_res["x"].apply(
+            lambda x: position_resolved(
+                *find_in_structure_with_inf(self.layers_thickness, x),
+                coh_tmm_data
+            )['absor']
+        )
+
+        bins = [
+            -self.plot_margin_l - 1, 0,
+            *numpy.cumsum(self.layers_thickness[1:])
+        ]
+        self.d_res["material"] = pandas.cut(
+            self.d_res["x"], bins=bins,
+            labels=self.layers_label
+        )
+        print(self.d_res)
+
+        self.d_res["absorption_cumulative"] = integrate.cumtrapz(
+            self.d_res["absorption"], self.d_res["x"], initial=0)
 
         self.transmission = coh_tmm_data["T"]
         self.reflection = coh_tmm_data["R"]
 
-    def plot(self, *, ax=None, show_layers=True, label=None):
+    def plot(self, *, ax=None, show_layers=True, label=None,
+             plot_cumulative=True):
         if ax is None:
-            self.fig, self.ax = plt.subplots(1, 1, figsize=(11.69, 8.27))
+            self.fig, self.ax = pyplot.subplots(1, 1, figsize=(11.69, 8.27))
             self.ax.set_ylabel('Absorption (%)')
             self.ax.set_xlabel('Depth (nm)')
         else:
             self.ax = ax
 
-        self.ax.plot(self.T, self.absorption * 100, label=label)
+        self.ax.plot(self.d_res.x, self.d_res.absorption * 100, label=label)
 
         if show_layers:
             t0 = 0
@@ -129,8 +148,20 @@ class TMM_stack:
 
                 t0 += t
 
+        if plot_cumulative:
+            self.ax2 = self.ax.twinx()
+
+            self.ax2.plot(
+                self.d_res.x, self.d_res.absorption_cumulative * 100,
+                color='r', label=label
+            )
+
     def mat2col(self, material):
         if material not in self._colormap:
             self._colormap[material] = self._sourcemap.pop(0)
 
         return self._colormap[material]
+
+    def calculate_material_absorption(self, material):
+
+        pass
