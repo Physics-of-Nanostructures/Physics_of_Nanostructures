d50bfa228f0d13fa21ac125e5fe991cfa6b084da
Casper Schippers <Casperschippers@hotmail.com>
2019-09-23 20:47:07 +0200

    Read file script

--- python_tools/fnatools/sempa_analysis/analysis.py
+++ python_tools/fnatools/sempa_analysis/analysis.py
@@ -1,3 +1,4 @@
+from .binaryFileReader import BinaryReader
 from dataclasses import dataclass
 
 
@@ -16,9 +17,65 @@ class SEMPA_Measurement:
         self.import_files()
 
     def import_files(self):
-        for filename in filenames:
-            self.import_file(filename)
+        for filename in self.filenames:
+            self.read_file(filename)
 
-    def import_file(self, file):
+    def read_file(self, file):
+        with open(file, "rb") as file:
+            print(type(file))
+            if not file.read(8) == b"FLAT0100":
+                raise ValueError("Unknown file-type")
 
-        with open(file, 'rb'):
+            reader = BinaryReader(file)
+
+            number_of_axes = reader.next_int()
+
+            for i in range(number_of_axes):
+                name = reader.next_string()
+                name_parent = reader.next_string()
+                name_units = reader.next_string()
+
+                clock_count = reader.next_int()
+                raw_start_val = reader.next_int()
+                raw_increment_val = reader.next_int()
+
+                physical_start_val = reader.next_double()
+                physical_increment_val = reader.next_double()
+
+                mirrored = reader.next_int()
+                table_count = reader.next_int()
+
+                if table_count != 0:
+                    raise NotImplementedError(
+                        "Non-zero table-count not yet implemented")
+
+            channel_name = reader.next_string()
+            transfer_function_name = reader.next_string()
+            unit_name = reader.next_string()
+
+            parameter_count = reader.next_int()
+            parameters = {
+                reader.next_string(): reader.next_double()
+                for _ in range(parameter_count)
+            }
+
+            number_of_data_views = reader.next_int()
+            data_view = [reader.next_int()
+                         for _ in range(number_of_data_views)]
+
+            time_stamp = datetime.datetime.fromtimestamp(reader.next_long())
+            information = {
+                info.split("=")[0]: info.split("=")[1]
+                for info in reader.next_string().split(";")
+            }
+
+            data_points = reader.next_int()
+            data_points_measured = reader.next_int()
+
+            if data_points != data_points_measured:
+                raise ValueError("Measurement not completed")
+
+            raw_data = [
+                reader.next_int()
+                for _ in range(data_points)
+            ]
---
+++ python_tools/fnatools/sempa_analysis/binaryFileReader.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+import struct
+import io
+
+@dataclass
+class BinaryReader:
+    filebuffer: io.BufferedReader
+
+    def next_string(self):
+        number_of_chars = self.next_int()
+        string = struct.unpack("cx" * number_of_chars,
+                               self.filebuffer.read(2 * number_of_chars))
+        string = b"".join(string)
+        string = string.decode("utf-8")
+        return string
+
+    def next_int(self):
+        value = struct.unpack("i", self.filebuffer.read(4))
+        return value[0]
+
+    def next_double(self):
+        value = struct.unpack("d", self.filebuffer.read(8))
+        return value[0]
+
+    def next_long(self):
+        value = struct.unpack("q", self.filebuffer.read(8))
+        return value[0]
