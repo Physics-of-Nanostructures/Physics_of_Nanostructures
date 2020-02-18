032c9cd538050495b8265aa01792a1ba0eed14ab
Casper Schippers <c.f.schippers@tue.nl>
2020-02-18 12:33:37 +0100

    renamed file

--- python_tools/fnatools/__init__.py
+++ python_tools/fnatools/__init__.py
@@ -2,6 +2,7 @@
 # This file is part of the fnatools package
 #
 from .basic_tools import strip_column_names, strip_unit
+from .pymeasurement import pyMeasurement
 
 name = 'fnatools'
 __version__ = '0.0.1'
